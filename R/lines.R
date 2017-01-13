2a88a9b4c3d9759b2c7b10e14ac1f517c1a6d43a
Kris Sankaran <sankaran.kris@gmail.com>
2017-01-12 17:50:03 -0800

    First pass at default options for grid of lines

---
+++ R/lines.R
@@ -0,0 +1,19 @@
+#! /usr/bin/env Rscript
+
+## File description -------------------------------------------------------------
+## Some functions for frequently used line plots with ggplot2.
+## author: kriss1@stanford.edu
+
+merge_line_opts <- function(opts = list()) {
+  default_opts <- list(
+    "x" = "x",
+    "y" = "y",
+    "col" = NULL,
+    "color_scale" = scale_color_brewer(palette = "Set2"),
+    "linetype" = NULL,
+    "facet_terms" = NULL
+    "row_order" = NULL,
+    "col_order" = NULL,
+    "theme_opts" = list()
+  )
+}
