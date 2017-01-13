8f479061a1896045c3704a30bc5c7ca5c0d46ca4
Kris Sankaran <sankaran.kris@gmail.com>
2017-01-12 18:57:53 -0800

    First pass gglines helper

--- R/lines.R
+++ R/lines.R
@@ -19,3 +19,27 @@ merge_line_opts <- function(opts = list()) {
   )
 }
 
+gglines <- function(plot_data, opts = list()) {
+  ## merge and prepare opts
+  opts <- merge_line_opts(opts)
+  aes_opts <- list(
+    "x" = opts$x,
+    "y" = opts$y,
+    "group" = opts$group,
+    "col" = opts$col
+  )
+  aes_opts <- aes_opts[!is.null(aes_opts)]
+
+  ## reorder levels for rows / columns
+  plot_data <- plot_data %>%
+    order_vars(
+      opts$facet_terms,
+      list(opts$row_order, opts$col_order)
+    )
+
+  ggplot(plot_data) +
+    geom_line(do.call(aes, aes_opts))
+
+
+
+}
