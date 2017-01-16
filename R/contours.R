29ccddb80615b217b3e480cc1d9ccc0fe8294620
Kris Sankaran <sankaran.kris@gmail.com>
2017-01-16 15:50:20 -0800

    Add contours example, and preliminary density code

--- R/contours.R
+++ R/contours.R
@@ -34,6 +34,37 @@ merge_contour_opts <- function(opts = list()) {
   modifyList(default_opts, opts)
 }
 
-ggcontours <- function() {
 
+#' @examples
+#' plot_data <- expand.grid(
+#'   "ix" = seq_len(1000),
+#'   "row" = c("A", "B", "C"),
+#'   "col" = c("alpha", "beta", "gamma")
+#' )
+#'
+#' plot_data <- cbind(
+#'   plot_data,
+#'   x = rnorm(9000),
+#'   y = rnorm(9000)
+#' )
+#' @importFrom magrittr %>%
+ggcontours <- function(plot_data, opts = list()) {
+  opts <- merge_contour_opts(opts)
+  aes_opts <- list(
+    "x" = opts$x,
+    "y" = opts$y,
+    "col" = opts$col
+  )
+
+  plot_data <- plot_data %>%
+    order_vars(opts$facet_terms, opts$facet_orders)
+
+  p <- ggplot(plot_data) +
+    stat_density2d(
+      do.call(aes_string, aes_opts)
+    ) +
+    opts$color_scale +
+    min_theme(opts$theme_opts)
+
+  add_facet(p, opts$facet_terms)
 }
