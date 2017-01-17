432967d6a41c4a29307981463821f9146d42bfdf
Kris Sankaran <sankaran.kris@gmail.com>
2017-01-16 17:33:14 -0800

    Special case for . in facet formula

--- R/facet.R
+++ R/facet.R
@@ -14,6 +14,9 @@
 order_vars <- function(X, vars, var_orders) {
   for (i in seq_along(vars)) {
 
+    ## . has special meaning in faceting formula, is not a column
+    if (vars[i] == ".") next
+
     ## If levels already exist, but are not specified in var_orders, use the
     ## existing ordering.
     if (i > length(var_orders)) {
